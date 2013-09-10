1ce270e10766e69c463c644e63643ed5ac701c92
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-10 10:39:55 -0400

    add doc

--- bin/ex01.dart
+++ bin/ex01.dart
@@ -1,7 +1,6 @@
 void main() {
   int number = 1;
-  for(int i = 1; i < 10; i++)
-  {
+  for(int i = 1; i < 10; i++) {
     print('${number}*${number}=${number*number}');
     number = number * 10 + 1;
   }
